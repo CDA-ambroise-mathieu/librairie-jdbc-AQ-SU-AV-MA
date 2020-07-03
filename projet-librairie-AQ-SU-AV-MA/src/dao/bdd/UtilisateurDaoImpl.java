7f97f4752c4db76a37d12e444b657b4d85588626
Alice Quinton <quinton.alice@gmail.com>
2020-07-03 15:41:56 +0200

    première partie UtilisateurDaoImpl
    première partie  méthodes UtilisateurDaoImpl (à tester)

--- projet-librairie-AQ-SU-AV-MA/src/dao/bdd/UtilisateurDaoImpl.java
+++ projet-librairie-AQ-SU-AV-MA/src/dao/bdd/UtilisateurDaoImpl.java
@@ -71,10 +71,15 @@ public class UtilisateurDaoImpl implements Dao<Utilisateur> {
 
 				// ajouter login et mdp
 				PreparedStatement ps = c
-						.prepareStatement("UPDATE utilisateur \r\n" + "SET nom= ? , prenom =?\r\n" + "WHERE id_utilisateur=?");
+						.prepareStatement("UPDATE utilisateur \r\n" + "SET nom= ? , prenom =? , role=? , num_compte=? , login=? , password=?\r\n," + "WHERE id_utilisateur=?");
 				ps.setString(1, pUtilisateur.getNom());
-				ps.setString(1, pUtilisateur.getPrenom());
-				ps.setInt(3, pUtilisateur.getId_utilisateur());
+				ps.setString(2, pUtilisateur.getPrenom());
+				ps.setString(3,  pUtilisateur.getRole());
+				ps.setInt(4,  pUtilisateur.getNum_compte());
+				ps.setString(5, pUtilisateur.getLogin());
+				ps.setString(6, pUtilisateur.getPassword());
+				ps.setInt(7, pUtilisateur.getId_utilisateur());
+				
 				ps.executeUpdate();
 			} catch (SQLException e) {
 				e.printStackTrace();
@@ -82,25 +87,46 @@ public class UtilisateurDaoImpl implements Dao<Utilisateur> {
 		}
 		return null;
 	}
-
+	// faire pareil que find by id mais find by login 
 	@Override
 	public Utilisateur findById(int pId_utilisateur) {
 		Utilisateur pUtilisateur = null;
 		Connection c = MyConnection.getConnection();
 		if (c != null) {
 			try {
-				PreparedStatement ps = c.prepareStatement("select * from utilisateur where num = ?;");
+				PreparedStatement ps = c.prepareStatement("select * from utilisateur where id_utilisateur= ?;");
 				ps.setInt(1, pId_utilisateur);
 				ResultSet r = ps.executeQuery();
 				if (r.next())
 					pUtilisateur = new Utilisateur(r.getInt("id_utilisateur"),
-							r.getString("prenom"), r.getString("nom"));
+							r.getString("prenom"), r.getString("nom"), r.getString("role"), r.getInt("num_compte"), r.getString("login"), r.getString("password"));
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
 		return pUtilisateur;
 	}
+	
+
+// find by login 
+@Override
+public Utilisateur findByLogin(String pLogin) {
+	Utilisateur pUtilisateur = null;
+	Connection c = MyConnection.getConnection();
+	if (c != null) {
+		try {
+			PreparedStatement ps = c.prepareStatement("select * from utilisateur where login= ?;");
+			ps.setString(1, pLogin);
+			ResultSet r = ps.executeQuery();
+			if (r.next())
+				pUtilisateur = new Utilisateur(r.getString("login"),
+						r.getString("prenom"), r.getString("nom"), r.getString("role"), r.getInt("num_compte"), r.getInt("id_utilisateur"), r.getString("password"));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	return pUtilisateur;
+}
 
 	@Override
 	public List<Utilisateur> getAll() {
@@ -120,6 +146,7 @@ public class UtilisateurDaoImpl implements Dao<Utilisateur> {
 					int id_utilisateur = retour.getInt("id_utilisateur");
 					String nom = retour.getString("nom");
 					String prenom = retour.getString("prenom");
+					String role = retour.getString
 
 					u1.setId_utilisateur(id_utilisateur);
 					u1.setNom(nom);
