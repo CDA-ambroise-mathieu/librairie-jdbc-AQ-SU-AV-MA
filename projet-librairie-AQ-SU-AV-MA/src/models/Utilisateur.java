c66987ccd73a89137b873ca9c4c693f44945b54c
Alice Quinton <quinton.alice@gmail.com>
2020-07-03 14:30:21 +0200

    premiers changements dans save pour UtilisateurDaoImpl
    premiers changements dans save pour UtilisateurDaoImp + uniformisation variables

--- projet-librairie-AQ-SU-AV-MA/src/models/Utilisateur.java
+++ projet-librairie-AQ-SU-AV-MA/src/models/Utilisateur.java
@@ -5,30 +5,23 @@ import lombok.Data;
 @Data
 public class Utilisateur {
 
-	private int id;
-	private boolean inscrit = false; // comme ça pas d'ambiguïté même si sa valeur par défaut est false
-	private boolean masque = false;
+	private int id_utilisateur;
 	private String prenom;
 	private String nom;
-
-	public Utilisateur(int pId, boolean pInscrit, String pPrenom, String pNom) {
+	private String role;
+	private int num_compte;
+	private String login;
+	private String password;
+	
+	public Utilisateur(int pId_utilisateur, String pPrenom, String pNom) {
 		super();
-		this.id = pId;
-		this.inscrit = pInscrit;
+		this.id_utilisateur = pId_utilisateur;
 		this.prenom = pPrenom;
 		this.nom = pNom;
 	}
 
-	public Utilisateur(boolean pInscrit, String pPrenom, String pNom) {
+	public Utilisateur(String pPrenom, String pNom) {
 		super();
-		this.inscrit = pInscrit;
-		this.prenom = pPrenom;
-		this.nom = pNom;
-	}
-
-	public Utilisateur(int pId, String pPrenom, String pNom) {
-		super();
-		this.id = pId;
 		this.prenom = pPrenom;
 		this.nom = pNom;
 	}
@@ -36,23 +29,13 @@ public class Utilisateur {
 	public Utilisateur() {
 		super();
 	}
-
-	public int getId() {
-		return id;
+	
+	public int getId_utilisateur() {
+		return id_utilisateur;
 	}
-
-	public void setId(int pId) {
-		id = pId;
+	public void setId(int pId_utilisateur) {
+		id_utilisateur = pId_utilisateur;
 	}
-
-	public boolean isInscrit() {
-		return inscrit;
-	}
-
-	public void setInscrit(boolean pInscrit) {
-		this.inscrit = pInscrit;
-	}
-
 	public String getPrenom() {
 		return prenom;
 	}
@@ -67,11 +50,6 @@ public class Utilisateur {
 
 	public void setNom(String pNom) {
 		this.nom = pNom;
-	}
-
-	@Override
-	public String toString() {
-		return "Utilisateur [inscrit=" + inscrit + ", prenom=" + prenom + ", nom=" + nom + "]";
 	}
 
 }
