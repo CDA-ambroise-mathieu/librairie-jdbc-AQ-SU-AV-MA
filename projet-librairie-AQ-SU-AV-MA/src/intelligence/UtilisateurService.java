23f71d7b151639bef2225dfb9f7040c164b92871
Samuel-umek <samuel.umek@gmail.com>
2020-07-03 13:45:38 +0200

    squelette Services créés

---
+++ projet-librairie-AQ-SU-AV-MA/src/intelligence/UtilisateurService.java
@@ -0,0 +1,17 @@
+package intelligence;
+
+
+public class UtilisateurService {
+	
+	public void incription() {
+		
+	}
+	
+	public void authentification() {
+		
+	}
+	
+	public void listerLivres() {
+		
+	}
+}
